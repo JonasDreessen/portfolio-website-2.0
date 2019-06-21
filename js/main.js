ab5820972d4afa63627ed5a3e27db0f4415f0d1f
JonasDreessen <dreessenjonas95@gmail.com>
2019-06-21 14:50:15 +0200

    add jquery library that is not working

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
-/*const animatedText_span = document.getElementById("animated-Text");
+// typing and deleting effect.
 
-var i = 0;
-var text1 = 'Jonas Dreessen'; //text 1
-const text2 = 'Front-end designer'
-const text3 = 'Back-end developer'
-const text4 = 'Full-stack developer'
-var speed = 100;
-
-function typeWriter() {
-  if (i < text1.length) {
-    animatedText_span.innerHTML += text1.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-    
-  } 
-}
-typeWriter(); */
 animatedText_span = document.getElementById("animated-Text");
 const words = ["Jonas Dreessen.", " a Front-end designer.", "a Back-end developer.", "a Full-stack expert."];
 let i = 0;
@@ -56,3 +40,9 @@ function deletingEffect() {
 };
 
 typingEffect();
+
+// Skills Bars
+$('.skill-percent').each(function(){
+    $(this).animate({
+      width:$(this).attr('data-percent')},"fast");
+    });
